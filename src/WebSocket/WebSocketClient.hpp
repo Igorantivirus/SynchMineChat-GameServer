a51ad9f2acb1486506099e00103a70e9510534e0
Игорь Антипов <igorantivirus@yandex.ru>
2025-07-05 23:06:10 +0700

    реализация клиент вебсокета

---
+++ src/WebSocket/WebSocketClient.hpp
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <string>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <atomic>
+#include <functional>
+
+#include <boost/beast.hpp>
+#include <boost/asio.hpp>
+
+#include "../Services/Service.hpp"
+
+#include "../AsynchContainers/AsynchQueue.hpp"
+
+#include "Utils.hpp"
+#include "WebSocketEvents.hpp"
+
+class WebSocketClient
+{
+public:
+    WebSocketClient() : ws_{io_}
+    {
+        ws_.set_option(websocket::stream_base::timeout::suggested(beast::role_type::client));
+        ws_.auto_fragment(true);
+    }
+    ~WebSocketClient()
+    {
+        disconnect();
+    }
+
+    WebSocketEvents& getEvents()
+    {
+        return events_;
+    }
+
+    bool connect(const std::string &ip, const std::string &port, const std::string &servPath)
+    {
+        std::lock_guard lg1(wsMut_);
+        std::lock_guard lg2(ioMut_);
+        try
+        {
+            auto const results = tcp::resolver(io_).resolve(ip, port);
+            net::connect(ws_.next_layer(), results);
+            ws_.handshake(ip, servPath);
+            isConnected_ = true;
+            readSetAsyncTask();
+            events_.onConnectF_();
+            return true;
+        }
+        catch (const beast::system_error &e)
+        {
+            isConnected_ = false;
+            return false;
+        }
+    }
+
+    void disconnect(websocket::close_code code = websocket::close_code::normal)
+    {
+        std::lock_guard lg1(wsMut_);
+        if (!isConnected_)
+            return;
+        try
+        {
+            ws_.close(code);
+        }
+        catch (...)
+        {}
+        events_.onCloseF_(code);
+        isConnected_ = false;
+    }
+
+    bool isConnected() const
+    {
+        return isConnected_;
+    }
+
+    void poll()
+    {
+        std::lock_guard lg1(ioMut_);
+        if(isConnected_)
+            io_.poll();
+    }
+
+    void sendMessage(const std::string& msg)
+    {
+        std::lock_guard lg1(wsMut_);
+        ws_.async_write(net::buffer(msg), [this](beast::error_code ec, std::size_t s){asyncWriteCallBack(ec, s);});
+    }
+
+    void ping()
+    {
+        std::lock_guard lg1(wsMut_);
+        ws_.async_ping("", [this](beast::error_code ec) {asyncPingCallBack(ec);});
+    }
+
+private:
+    net::io_context io_;
+    websocket::stream<tcp::socket> ws_;
+    
+    std::mutex ioMut_;
+    std::mutex wsMut_;
+
+    std::atomic<bool> isConnected_ = false;
+
+    beast::flat_buffer readBuffer;
+    WebSocketEvents events_;
+
+private:
+
+    void readSetAsyncTask()
+    {
+        ws_.async_read(readBuffer, [this](beast::error_code ec, size_t s){asyncReadCallBeck(ec, s);});
+    }
+
+    void asyncPingCallBack(beast::error_code ec)
+    {
+        if (ec)
+            disconnect(websocket::close_code::protocol_error); 
+    }
+
+    void asyncWriteCallBack(beast::error_code ec, std::size_t)
+    {
+        if (ec)
+            disconnect(websocket::close_code::internal_error);
+    }
+
+    void asyncReadCallBeck(beast::error_code ec, size_t)
+    {
+        if (!ec)
+        {
+            events_.onMessageF_(beast::buffers_to_string(readBuffer.data()));
+            if(isConnected_)
+                readSetAsyncTask();  // Запускаем следующий цикл чтения
+        }
+        else
+        {
+            disconnect(websocket::close_code::internal_error);//Просто закроем соединение
+        }
+        readBuffer.consume(readBuffer.size()); // Очищаем буфер
+    }
+
+};
