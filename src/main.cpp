4e4a4f70b718978b32bf027cd08477c9ea3507ec
Игорь Антипов <igorantivirus@yandex.ru>
2025-07-06 11:30:05 +0700

    Тесты

--- src/main.cpp
+++ src/main.cpp
@@ -21,30 +21,61 @@ void onDisconnect(websocket::close_code)
     std::cout << "Disconnect\n";
 }
 
+#include <nlohmann/json.hpp>
+
+#include "Parser/Utils.hpp"
+
+
+#include "Parser/Parser.hpp"
+
 int main()
 {
-    Service::log.setEntryToConsole(true);
+    Service::staticConstruct("resources/MainConfig.json");
 
-    WebSocketClient wb;
-    wb.getEvents().onMessage(onMessage);
-    wb.getEvents().onClose(onDisconnect);
-    wb.getEvents().onConnect(onConnect);
+    MinecraftLogParser parser;
+    AsynchSafelyQueue<LogMessage> queue;
 
-    wb.connect("localhost", "18080", "/ws");
+    parser.parse(queue);
 
-    std::thread t([&]()
+    while(!queue.empty())
     {
-        while(wb.isConnected())
-            wb.poll();
-    });
-
-    while(wb.isConnected())
-    {   
-        wb.sendMessage(input("Input: "));
+        LogMessage msg = queue.pop();
+        // std::cout << std::format
+        std::cout << toString(msg.type) << ' ' << msg.playerName << ' ' << msg.msg << '\n';   
     }
 
 
-    t.join();
+    // DateTime dt = DateTime::fromLogString("[05Jul2025 08:33:37.817]");
+    // std::cout << dt.date << ' ' << dt.time << '\n';
+    // std::cout << DateTime::toLogString(dt) << '\n';
+
+    // 
+    // Service::log.setEntryToConsole(true);
+
+    // const constexpr int s = sizeof("[05Jul2025 08:33:37.817]");
+
+    // Service::log.log("My info", LogLevel::Critical);
+
+    // WebSocketClient wb;
+    // wb.getEvents().onMessage(onMessage);
+    // wb.getEvents().onClose(onDisconnect);
+    // wb.getEvents().onConnect(onConnect);
+
+    // wb.connect("localhost", "18080", "/ws");
+
+    // std::thread t([&]()
+    // {
+    //     while(wb.isConnected())
+    //         wb.poll();
+    // });
+
+    // while(wb.isConnected())
+    // {   
+    //     wb.sendMessage(input("Input: "));
+    // }
+
+
+    // t.join();
 
     return 0;
 }
