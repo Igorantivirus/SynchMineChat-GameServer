ebc1d2d00c560fbaece6c2ba899f29ff4c4a03cc
Игорь Антипов <igorantivirus@yandex.ru>
2025-07-06 13:52:18 +0700

    Завершён класс майнкрафт чата

--- src/MinecraftChat/MinecraftChatAPI.hpp
+++ src/MinecraftChat/MinecraftChatAPI.hpp
@@ -1,22 +1,56 @@
 #pragma once
 
-#include "../AsynchContainers/AsynchQueue.hpp"
-
-#include "../Parser/Parser.hpp"
+#include <string>
 
 #include <rconpp/rcon.h>
+
+#include "../Services/Service.hpp"
+#include "../AsynchContainers/AsynchQueue.hpp"
+#include "../Parser/Parser.hpp"
+#include "Messange.hpp"
 
 class MinecraftChatAPI
 {
 public:
+    MinecraftChatAPI() :
+        rconClient_{Service::config.MINECRAFT_SERVER_IP, Service::config.MINECRAFT_SERVER_PORT, Service::config.MINECRAFT_SERVER_PASSWORD }
+    {
+        rconClient_.on_log = std::bind(&MinecraftChatAPI::onLog, this, std::placeholders::_1);
+        onResponseF_ = std::bind(&MinecraftChatAPI::onResponse, this, std::placeholders::_1);
+    }
 
+    void sendMessage(const Message& message)
+    {
+        std::string text = '<' + message["userName"] + '>' + ' ' + message[text];
+        std::string command = "/tellraw @a [\"" + text + "\"]";
+        rconClient_.send_data(command, 3, rconpp::data_type::SERVERDATA_EXECCOMMAND, onResponseF_);
+    }
 
-
+    AsynchSafelyQueue<LogMessage>& getNextMessages()
+    {
+        parser_.parse(messages_);
+        return messages_;
+    }
 
 private:
 
+    MinecraftLogParser parser_;
+    rconpp::rcon_client rconClient_;
 
+    AsynchSafelyQueue<LogMessage> messages_;
 
+private:
+    
+    std::function<void(const rconpp::response&)> onResponseF_;
 
+    void onLog(const std::string_view& log)
+    {
+        Service::log.log("Message from RCON: " + std::string(log), LogLevel::Info);
+    }
+
+    void onResponse(const rconpp::response& response)
+    {
+        Service::log.log("Response on RCON send message: " + response.data, LogLevel::Info);
+    }
 
 };
