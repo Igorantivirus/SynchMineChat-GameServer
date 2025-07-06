77125e71c48d9da99106dd7d3b2017340bc716ed
Игорь Антипов <igorantivirus@yandex.ru>
2025-07-06 11:07:40 +0700

    Добавлен парс строки лога

--- src/Parser/LogMessage.hpp
+++ src/Parser/LogMessage.hpp
@@ -13,7 +13,53 @@ enum class LogType : unsigned char
 
 struct LogMessage
 {
-    LogType type;
+    LogType type = LogType::none;
     std::string playerName;
     std::string msg;
+
+public:
+
+    bool parseFromString(const std::string& str)
+    {
+        std::size_t pos{};
+        if (pos = str.find("[Not Secure]"); pos != std::string::npos)
+            return parseMsg(str, pos);
+        if (pos = str.find("joined the game"); pos != std::string::npos)
+            return parseJoin(str, pos);
+        if (pos = str.find("left the game"); pos != std::string::npos)
+            return parseLeft(str, pos);
+        return false;
+    }   
+
+private:
+
+    bool parseMsg(const std::string& str, const std::size_t pos)
+    {
+        std::size_t begin = str.find('<', pos) + 1;
+        std::size_t end = str.find('>', pos);
+        playerName = str.substr(begin, end - begin);
+        msg = str.substr(end + 2);
+        type = LogType::message;
+        return true;
+    }
+    bool parseJoin(const std::string& str, const std::size_t pos)
+    {
+        std::size_t begin = 0;
+        for(begin = pos - 1; begin != 0 && str[begin - 1] != ' ';--begin);
+        std::size_t end = pos - 1;
+        playerName = str.substr(begin, end - begin);
+        msg = "";
+        type = LogType::join;
+        return true;
+    }
+    bool parseLeft(const std::string& str, const std::size_t pos)
+    {
+        std::size_t begin = 0;
+        for(begin = pos - 1; begin != 0 && str[begin - 1] != ' ';--begin);
+        std::size_t end = pos - 1;
+        playerName = str.substr(begin, end - begin);
+        msg = "";
+        type = LogType::left;
+        return true;
+    }
 };
