1dc9dd32b9a04f9d2a455001c0d5c84534d2464b
Игорь Антипов <igorantivirus@yandex.ru>
2025-07-07 23:22:13 +0700

    Исправлена ошибка в split

--- src/StringUtils.hpp
+++ src/StringUtils.hpp
@@ -10,7 +10,7 @@ namespace StringUtils
     {
         std::list<std::string> res;
         std::size_t last = 0;
-        for(std::size_t ind = s.find(c); ind != std::string::npos;last = ind, ind = s.find(c, ind + 1))
+        for(std::size_t ind = s.find(c); ind != std::string::npos;last = ind + 1, ind = s.find(c, last))
             res.push_back(s.substr(last, ind - last));
         res.push_back(s.substr(last));
         return res;
